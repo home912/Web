483c1ca4a96b2076479432e6c757f305bca07f57
home912 <mr.vladbutorin@mail.ru>
2023-03-31 23:35:15 +0700

    Add new page and add amination for simple items

--- js/index.js
+++ js/index.js
@@ -19,16 +19,16 @@ $(document).ready(function () {
 });
 
 
-const animItems = document.querySelectorAll("._anim-items"); // Элементы которые нужно анимировать
-if (animItems.length > 0) {
+const animTitle = document.querySelectorAll("._anim-title"); // Элементы которые нужно анимировать
+if (animTitle.length > 0) {
   window.addEventListener('scroll', animOnScroll);
   /* Что происходит: Для каждого элемента который имеет класс '_anim-items' при достижении 1/4 его
   высоты, либо 1/4 высоты окна браузера(если высота объекта выше высоты окна браузера) ему 
   добавляется класс '_active'. Если мы недокрутили до елемента, либо перекрутили то у него класс
   '_active' убирается. */
   function animOnScroll() {
-    for (let index = 0; index < animItems.length; index++) {
-      const animItem = animItems[index]; // Получаем каждый объект отдельно
+    for (let index = 0; index < animTitle.length; index++) {
+      const animItem = animTitle[index]; // Получаем каждый объект отдельно
       const animItemHeight = animItem.offsetHeight; // Получаем высоту объекта
       const animItemOffset = offset(animItem).top; // Позиция объекта относительно верха
       const animStart = 4; // Коэффициент резулирующий момент старта анимации
@@ -38,17 +38,17 @@ if (animItems.length > 0) {
 
       // Для случая когда анимированный объект выше по высоте чем окно браузера
       if (animItemHeight > window.innerHeight) {
-        animItemPoint = window.innerHeight - window.innerHeight / 4;
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
       }
 
       // Добавляем элементам класс при определенных условиях
       if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
-        animItem.classList.add('_active');
+        animItem.classList.add('_active-anim');
       } else { // Убрать класс нужно для повторной анимации объекта
         /* Наличие у объекта класса '_anim-no-hide' говорит о том что не нужно объект  
         повторно анимировать если опять на него проскролили */
-        if (!animItem.classList.contains('_anim-no-hide')) {
-          animItem.classList.remove('_active');
+        if (!animItem.classList.contains('_anim-no-hide-title')) {
+          animItem.classList.remove('_active-anim');
         }
       }
     }
@@ -63,4 +63,50 @@ if (animItems.length > 0) {
   setTimeout(function () {
     animOnScroll();
   }, 300);
+}
+
+const animItems = document.querySelectorAll("._anim-item"); // Элементы которые нужно анимировать
+if (animItems.length > 0) {
+    window.addEventListener('scroll', animOnScroll);
+    /* Что происходит: Для каждого элемента который имеет класс '_anim-item' при достижении 1/4 его
+    высоты, либо 1/4 высоты окна браузера(если высота объекта выше высоты окна браузера) ему 
+    добавляется класс '_active'. Если мы недокрутили до елемента, либо перекрутили то у него класс
+    '_active' убирается. */
+    function animOnScroll() {
+        for (let index = 0; index < animItems.length; index++) {
+            const animItem = animItems[index]; // Получаем каждый объект отдельно
+            const animItemHeight = animItem.offsetHeight; // Получаем высоту объекта
+            const animItemOffset = offset(animItem).top; // Позиция объекта относительно верха
+            const animStart = 4; // Коэффициент резулирующий момент старта анимации
+
+            // Настройка момента старта анимации
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            // Для случая когда анимированный объект выше по высоте чем окно браузера
+            if (animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / 4;
+            }
+
+            // Добавляем элементам класс при определенных условиях
+            if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('_active');
+            } else { // Убрать класс нужно для повторной анимации объекта
+                /* Наличие у объекта класса '_anim-no-hide' говорит о том что не нужно объект  
+                повторно анимировать если опять на него проскролили */
+                if (!animItem.classList.contains('_anim-no-hide')) {
+                    animItem.classList.remove('_active');
+                }
+            }
+        }
+    }
+    function offset(el) {// Корректно и кроссбраузерно выщитывает позиция объекта относительно верха
+        const rect = el.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+    }
+
+    setTimeout(function () {
+        animOnScroll();
+    }, 300);
 }
