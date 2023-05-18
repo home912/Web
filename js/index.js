caad4406682f49fa9a17ab024d861870ea0fcc9c
home912 <mr.vladbutorin@mail.ru>
2023-05-18 19:18:41 +0700

    Fix navbar

--- js/index.js
+++ js/index.js
@@ -8,10 +8,17 @@ window.onscroll = function () {
   var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
+    document.getElementById("myBtn").style.display = "block";
   } else {
     document.getElementById("navbar").style.top = "-60px";
+
   }
   prevScrollpos = currentScrollPos;
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
 }
 
 /* функция открытия меню-бургер */
@@ -71,63 +78,48 @@ if (animTitle.length > 0) {
 
 const animItems = document.querySelectorAll("._anim-item"); // Элементы которые нужно анимировать
 if (animItems.length > 0) {
-    window.addEventListener('scroll', animOnScroll);
-    /* Что происходит: Для каждого элемента который имеет класс '_anim-item' при достижении 1/4 его
-    высоты, либо 1/4 высоты окна браузера(если высота объекта выше высоты окна браузера) ему 
-    добавляется класс '_active'. Если мы недокрутили до елемента, либо перекрутили то у него класс
-    '_active' убирается. */
-    function animOnScroll() {
-        for (let index = 0; index < animItems.length; index++) {
-            const animItem = animItems[index]; // Получаем каждый объект отдельно
-            const animItemHeight = animItem.offsetHeight; // Получаем высоту объекта
-            const animItemOffset = offset(animItem).top; // Позиция объекта относительно верха
-            const animStart = 4; // Коэффициент резулирующий момент старта анимации
+  window.addEventListener('scroll', animOnScroll);
+  /* Что происходит: Для каждого элемента который имеет класс '_anim-item' при достижении 1/4 его
+  высоты, либо 1/4 высоты окна браузера(если высота объекта выше высоты окна браузера) ему 
+  добавляется класс '_active'. Если мы недокрутили до елемента, либо перекрутили то у него класс
+  '_active' убирается. */
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index]; // Получаем каждый объект отдельно
+      const animItemHeight = animItem.offsetHeight; // Получаем высоту объекта
+      const animItemOffset = offset(animItem).top; // Позиция объекта относительно верха
+      const animStart = 4; // Коэффициент резулирующий момент старта анимации
 
-            // Настройка момента старта анимации
-            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      // Настройка момента старта анимации
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
-            // Для случая когда анимированный объект выше по высоте чем окно браузера
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart;
-            }
+      // Для случая когда анимированный объект выше по высоте чем окно браузера
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
 
-            // Добавляем элементам класс при определенных условиях
-            if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
-                animItem.classList.add('_active');
-            } else { // Убрать класс нужно для повторной анимации объекта
-                /* Наличие у объекта класса '_anim-no-hide' говорит о том что не нужно объект  
-                повторно анимировать если опять на него проскролили */
-                if (!animItem.classList.contains('_anim-no-hide')) {
-                    animItem.classList.remove('_active');
-                }
-            }
+      // Добавляем элементам класс при определенных условиях
+      if ((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('_active');
+      } else { // Убрать класс нужно для повторной анимации объекта
+        /* Наличие у объекта класса '_anim-no-hide' говорит о том что не нужно объект  
+        повторно анимировать если опять на него проскролили */
+        if (!animItem.classList.contains('_anim-no-hide')) {
+          animItem.classList.remove('_active');
         }
+      }
     }
-    function offset(el) {// Корректно и кроссбраузерно выщитывает позиция объекта относительно верха
-        const rect = el.getBoundingClientRect(),
-            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-    }
+  }
+  function offset(el) {// Корректно и кроссбраузерно выщитывает позиция объекта относительно верха
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
 
-    setTimeout(function () {
-        animOnScroll();
-    }, 300);
+  setTimeout(function () {
+    animOnScroll();
+  }, 300);
 }
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
