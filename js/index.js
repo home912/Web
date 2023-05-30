0d7cee49f761f42a2ae0f6a5ecb13ef0f355e0a5
home912 <mr.vladbutorin@mail.ru>
2023-05-30 17:54:05 +0700

    Add slider img

--- js/index.js
+++ js/index.js
@@ -124,22 +124,13 @@ if (animItems.length > 0) {
 
 const swiper = new Swiper('.swiper', {
   // Optional parameters
-  direction: 'vertical',
+  direction: 'horizontal',
   loop: true,
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    clickable: true,
   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
